3e33d0b01aebc2ddf1a857e7f581ae8da1827d9b
SamerYaserr <samersmora1290@gmail.com>
2025-12-24 17:09:05 +0200

    feat(bookings): add booking details page and navigation
    - Added actions menu to booking rows with navigation to booking details.
    - Introduced dynamic booking route (/bookings/:bookingId).
    - Implemented Booking page and BookingDetail feature.
    - Added useBooking hook for fetching single booking data.
    - Displayed booking status, metadata, and loading state.
    - Integrated spinner and back navigation for improved UX.

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 import { format, isToday } from "date-fns";
+import { HiEye } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
 
 import Tag from "../../ui/Tag";
 import Table from "../../ui/Table";
+import Menus from "../../ui/Menus";
 
 import { formatCurrency } from "../../utils/helpers";
 import { formatDistanceFromNow } from "../../utils/helpers";
@@ -48,6 +51,8 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate();
+
   const statusToTagName = {
     unconfirmed: "blue",
     "checked-in": "green",
@@ -71,7 +76,8 @@ function BookingRow({
           &rarr; {numNights} night stay
         </span>
         <span>
-          {format(new Date(startDate), "MMM dd yyyy")} &mdash;{" "}
+          {format(new Date(startDate), "MMM dd yyyy")} &mdash; Loaded:
+          3%Progress bar: 12.30%
           {format(new Date(endDate), "MMM dd yyyy")}
         </span>
       </Stacked>
@@ -79,6 +85,18 @@ function BookingRow({
       <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
 
       <Amount>{formatCurrency(totalPrice)}</Amount>
+
+      <Menus.Menu>
+        <Menus.Toggle id={bookingId} />
+        <Menus.List id={bookingId}>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+          >
+            See Details
+          </Menus.Button>
+        </Menus.List>
+      </Menus.Menu>
     </Table.Row>
   );
 }
