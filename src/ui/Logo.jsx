e93935b395dd0209a80915a612436dcc21dd4818
SamerYaserr <samersmora1290@gmail.com>
2026-01-01 01:12:55 +0200

    feat(ui): sync favicon with dark mode logo

--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import styled from "styled-components";
 
 import { useDarkMode } from "../context/DarkModeContext";
@@ -14,6 +15,25 @@ const Img = styled.img`
 function Logo() {
   const { isDarkMode } = useDarkMode();
   const src = isDarkMode ? "/logo-light.png" : "/logo-dark.png";
+
+  useEffect(
+    function () {
+      // Try to find an existing favicon
+      let link = document.querySelector("link[rel~='icon']");
+
+      // If it doesn't exist, create it and append it to the <head>
+      if (!link) {
+        link = document.createElement("link");
+        link.type = "image/png";
+        link.rel = "icon";
+        document.getElementsByTagName("head")[0].appendChild(link);
+      }
+
+      // Set the source
+      link.href = src;
+    },
+    [src]
+  );
 
   return (
     <StyledLogo>
